0caa5a580e9af91d847c307be8bd9e763cba882e
Joao Vicente <vicentejo@dnb.com>
2015-04-19 20:14:51 +0100

    making POST /tracy/segment response asynchronous of processing

--- src/main/java/com/apm4all/tracy/RouteBuilder.java
+++ src/main/java/com/apm4all/tracy/RouteBuilder.java
@@ -7,18 +7,24 @@ public class RouteBuilder extends SpringRouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		from("restlet:http://localhost:8050/tracy/segment?restletMethod=POST")
-			.to("seda:tracySegmentProcessor")
+			// Tracy publishing should never block the sender
+			// waitForTaskToComplete allows endpoint to respond
+			// without having to wait until tracy is finally stored
+			.to("seda:tracySegmentProcessor?waitForTaskToComplete=Never")
+			//TODO: Return taskId-component as reference with HTTP 202 code
 			.setBody(simple("{\"tracySegmentId\":\"123456\"}"));
 		
 		from("seda:tracySegmentProcessor")
 			.unmarshal().json(JsonLibrary.Jackson)
 			.transform().simple("${body[tracySegment]}")
-			// TODO: Validate tracySegment (i.e. filter)
+			// TODO: Validate tracySegment messages
+			// TODO: Send invalid segments to audit log
 			.split(body())
 			.to("seda:storeTracy");
 		
 		from("seda:storeTracy")
 			// TODO: Store Tracy frames in repository
-			.to("stream:out");
+			.delay(0);
+//			.log("${body}");
 	}
 }

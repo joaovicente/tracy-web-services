a52d324092bf09c63be60fbb7eba95bb60e492db
Joao Vicente <vicentejo@dnb.com>
2015-09-04 23:00:12 +0100

    TaskMeasurement converted to generics

--- src/main/java/com/apm4all/tracy/TaskMeasurement.java
+++ src/main/java/com/apm4all/tracy/TaskMeasurement.java
@@ -1,76 +1,72 @@
 package com.apm4all.tracy;
 
-@SuppressWarnings("unused")
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+
 public class TaskMeasurement {
-	private DapdexTimechart dapdexTimechart = new DapdexTimechart(); 
-	private VitalsTimechart vitalsTimechart = new VitalsTimechart();
-	private LatencyHistogram latencyHistogram = new LatencyHistogram();
-    private String measurement =  
-    "{"
-    + "'dapdexTimechart':{"
-    	+ "'timeSequence':[1439312400000,1439312401900,1439312403800,1439312405700,1439312407600,1439312409500,1439312411400,1439312413300,1439312415200,1439312417100,1439312419000,1439312420900,1439312422800,1439312424700,1439312426600,1439312428500],"
-    	+ "'dapdexScores':[0.99,0.98,0.99,0.93,0.94,0.97,0.95,0.97,0.83,0.93,0.97,0.94,0.96,0.98,0.95,0.96]"
-    + "},"
-    + "'vitalsTimechart\":{"
-    	+ "'timeSequence':[1439312400000,1439312401900,1439312403800,1439312405700,1439312407600,1439312409500,1439312411400,1439312413300,1439312415200,1439312417100,1439312419000,1439312420900,1439312422800,1439312424700,1439312426600,1439312428500],"
-    	+ "'count':[200,243,254,234,253,265,245,247,765,243,265,273,247,256,236,245],"
-    	+ "'errors':[1,2,1,2,1,2,1,2,4,1,2,1,2,1,2,1],"
-    	+ "'p95':[110,132,141,143,151,134,123,131,111,125,123,143,122,156,116,145]},"
-    + "'latencyHistogram':{"
-    	+ "'bins':['>1200','1140-1200','1080-1139','1020-1079','960-1019','900-959','840-899','780-839','720-779','660-719','600-659','540-599','480-539','420-479','360-419','300-359','240-299','180-239','120-179','60-119','0-59'],"
-    	+ "'rttZone':['Frustrated','Tolerating','Tolerating','Tolerating','Tolerating','Tolerating','Tolerating','Tolerating','Tolerating','Tolerating','Tolerating','Tolerating','Tolerating','Tolerating','Tolerating','Tolerating','Satisfied','Satisfied','Satisfied','Satisfied','Satisfied'],"
-    	+ "'count':[4,8,22,22,22,22,76,89,44,134,134,134,178,178,268,447,670,1548,312,89,44]}"
-    + "}";
+	private LinkedHashMap<String, Object> dapdexTimechart;
+	private LinkedHashMap<String, Object> vitalsTimechart;
+	private LinkedHashMap<String, Object> latencyHistogram;
+    public TaskMeasurement()	{
+    	// APDEX timechart
+    	dapdexTimechart = new LinkedHashMap<String, Object>();
+    	ArrayList<Long> timeSequence = new ArrayList<Long>(Arrays.asList(new Long[]
+    			{1439312400000L,1439312401900L,1439312403800L,1439312405700L,
+    			1439312407600L,1439312409500L,1439312411400L,1439312413300L,
+    			1439312415200L,1439312417100L,1439312419000L,1439312420900L,
+    			1439312422800L,1439312424700L,1439312426600L,1439312428500L}
+    	));;
+    	ArrayList<Double> dapdexScores = new ArrayList<Double>(Arrays.asList(new Double[]
+    			{0.99,0.98,0.99,0.93,0.94,0.97,0.95,0.97,0.83,0.93,0.97,0.94,0.96,0.98,0.95,0.96}
+    	));;
+    	dapdexTimechart.put("timeSequence", timeSequence);
+    	dapdexTimechart.put("dapdexScores", dapdexScores);
+    	
+    	// Vitals Timechart
+    	vitalsTimechart = new LinkedHashMap<String, Object>();
+    	ArrayList<Integer> count = new ArrayList<Integer>(Arrays.asList(new Integer[]
+    			 {200,243,254,234,253,265,245,247,765,243,265,273,247,256,236,245}
+    	));;
+    	ArrayList<Integer> errors = new ArrayList<Integer>(Arrays.asList(new Integer[]
+    			{1,2,1,2,1,2,1,2,4,1,2,1,2,1,2,1}
+    	));
+    	ArrayList<Integer> p95 = new ArrayList<Integer>(Arrays.asList(new Integer[]
+    			{110,132,141,143,151,134,123,131,111,125,123,143,122,156,116,145}
+    	));;
+    	vitalsTimechart.put("timeSequence", timeSequence);
+    	vitalsTimechart.put("count", count);
+    	vitalsTimechart.put("errors", errors);
+    	vitalsTimechart.put("p95", p95);
+    	
+    	// Latency histogram
+    	latencyHistogram = new LinkedHashMap<String, Object>();
+    	ArrayList<String> bins = new ArrayList<String>(Arrays.asList(new String[]
+    			{"1200","1140-1200","1080-1139","1020-1079","960-1019","900-959","840-899","780-839","720-779","660-719"
+    			,"600-659","540-599","480-539","420-479","360-419","300-359","240-299","180-239","120-179","60-119","0-59"}
+    	));;
+    	ArrayList<String> rttZone = new ArrayList<String>(Arrays.asList(new String[]
+    			{"Frustrated","Tolerating","Tolerating","Tolerating","Tolerating",
+    			"Tolerating","Tolerating","Tolerating","Tolerating","Tolerating"
+    			,"Tolerating","Tolerating","Tolerating","Tolerating","Tolerating"
+    			,"Tolerating","Satisfied","Satisfied","Satisfied","Satisfied","Satisfied"}
+    	));;
+    	ArrayList<Integer> lhCount = new ArrayList<Integer>(Arrays.asList(new Integer[]
+    			{4,8,22,22,22,22,76,89,44,134,134,134,178,178,268,447,670,1548,312,89,44}
+    	));
+    	latencyHistogram.put("timeSequence", timeSequence);
+    	latencyHistogram.put("bins", bins);
+    	latencyHistogram.put("rttZone", rttZone);
+    	latencyHistogram.put("count", lhCount);
+    }
     
-    private class DapdexTimechart {
-    	private Long[] timeSequence = {1439312400000L,1439312401900L,1439312403800L,1439312405700L,1439312407600L,1439312409500L,1439312411400L,1439312413300L,1439312415200L,1439312417100L,1439312419000L,1439312420900L,1439312422800L,1439312424700L,1439312426600L,1439312428500L};
-    	private Double[] dapdexScores = {0.99,0.98,0.99,0.93,0.94,0.97,0.95,0.97,0.83,0.93,0.97,0.94,0.96,0.98,0.95,0.96};
-		public Long[] getTimeSequence() {
-			return timeSequence;
-		}
-		public Double[] getDapdexScores() {
-			return dapdexScores;
-		}
-    }
-    private class VitalsTimechart {
-    	private Long[] timeSequence = {1439312400000L,1439312401900L,1439312403800L,1439312405700L,1439312407600L,1439312409500L,1439312411400L,1439312413300L,1439312415200L,1439312417100L,1439312419000L,1439312420900L,1439312422800L,1439312424700L,1439312426600L,1439312428500L};
-    	private int[] count = {200,243,254,234,253,265,245,247,765,243,265,273,247,256,236,245};
-    	private int[] errors = {1,2,1,2,1,2,1,2,4,1,2,1,2,1,2,1};
-    	private int[] p95 = {110,132,141,143,151,134,123,131,111,125,123,143,122,156,116,145};
-		public Long[] getTimeSequence() {
-			return timeSequence;
-		}
-		public int[] getCount() {
-			return count;
-		}
-		public int[] getErrors() {
-			return errors;
-		}
-		public int[] getP95() {
-			return p95;
-		}
-    }
-    private class LatencyHistogram {
-    	private String[] bins = {"1200","1140-1200","1080-1139","1020-1079","960-1019","900-959","840-899","780-839","720-779","660-719","600-659","540-599","480-539","420-479","360-419","300-359","240-299","180-239","120-179","60-119","0-59"};
-    	private String[] rttZone = {"Frustrated","Tolerating","Tolerating","Tolerating","Tolerating","Tolerating","Tolerating","Tolerating","Tolerating","Tolerating","Tolerating","Tolerating","Tolerating","Tolerating","Tolerating","Tolerating","Satisfied","Satisfied","Satisfied","Satisfied","Satisfied"};
-    	private int[] count = {4,8,22,22,22,22,76,89,44,134,134,134,178,178,268,447,670,1548,312,89,44};
-		public String[] getBins() {
-			return bins;
-		}
-		public String[] getRttZone() {
-			return rttZone;
-		}
-		public int[] getCount() {
-			return count;
-		}
-    }
-	public DapdexTimechart getDapdexTimechart() {
+	public LinkedHashMap<String, Object> getDapdexTimechart() {
 		return dapdexTimechart;
 	}
-	public VitalsTimechart getVitalsTimechart() {
+	public LinkedHashMap<String, Object> getVitalsTimechart() {
 		return vitalsTimechart;
 	}
-	public LatencyHistogram getLatencyHistogram() {
+	public LinkedHashMap<String, Object> getLatencyHistogram() {
 		return latencyHistogram;
 	}
 }

88006fc95e427aceed9abb867db4fa4920916488
Joao Vicente <vicentejo@dnb.com>
2016-04-02 23:18:41 +0100

    Created sample queries for task analysis

--- src/test/java/com/apm4all/tracy/EsQuerySamples.java
+++ src/test/java/com/apm4all/tracy/EsQuerySamples.java
@@ -1,6 +1,9 @@
 package com.apm4all.tracy;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
@@ -70,6 +73,103 @@ public class EsQuerySamples {
         contentBuilder.endObject();
         System.out.println( contentBuilder.string());
 	}
+
+    @Test
+    public void testFindTasksMatchingCriteria() throws IOException {
+        TaskConfig taskConfig = new TaskConfig("myApplication", "myTask");
+        TimeFrame timeFrame = new TimeFrame(null, null, null, taskConfig);
+
+        System.out.println(timeFrame);
+        long earliest = timeFrame.getEarliest();
+        long latest = timeFrame.getLatest();
+        String taskDefiningFilter = taskConfig.getDefiningFilter();
+
+        BoolQueryBuilder queryBuilder1 = QueryBuilders.boolQuery()
+                .must(QueryBuilders.rangeQuery("@timestamp").gt(earliest).lt(latest))
+                .must(QueryBuilders.queryStringQuery(taskDefiningFilter));
+
+        XContentBuilder contentBuilder = XContentFactory.jsonBuilder().startObject().field("query");
+        queryBuilder1.toXContent(contentBuilder, null);
+        contentBuilder.field("size", 20);
+
+        // "sort": [ { "msecElapsed" : {"unmapped_type" : "long"} } ]
+        HashMap<String, String> unmappedType = new HashMap<String, String>();
+        unmappedType.put("unmapped_type", "long");
+        HashMap<String, Map> msecElapsed = new HashMap<String, Map>();
+        msecElapsed.put("msecElapsed", unmappedType);
+        ArrayList sortList = new ArrayList();
+        sortList.add(msecElapsed);
+        contentBuilder.field("sort", sortList);
+
+        //"fields": ["taskId"]
+        ArrayList fieldsArray = new ArrayList();
+        fieldsArray.add("taskId");
+        contentBuilder.field("fields", fieldsArray);
+
+        contentBuilder.endObject();
+        System.out.println( contentBuilder.string());
+//        {
+//            "sort": [
+//            {
+//                "msecElapsed": {
+//                "unmapped_type": "long"
+//            }
+//            }
+//            ],
+//            "query": {
+//            "bool": {
+//                "must": [
+//                {
+//                    "range": {
+//                    "@timestamp": {
+//                        "from": 0,
+//                                "to": 1459376010000,
+//                                "include_lower": false,
+//                                "include_upper": false
+//                    }
+//                }
+//                },
+//                {
+//                    "query_string": {
+//                    "query": "component:\"hello-tracy\" AND label:\"outer\""
+//                }
+//                }
+//                ]
+//            }
+//        },
+//            "fields": [
+//            "taskId"
+//            ]
+//        }
+    }
+
+    @Test
+    public void testRetrieveTracyForTaskList() throws IOException {
+        TaskConfig taskConfig = new TaskConfig("myApplication", "myTask");
+        TimeFrame timeFrame = new TimeFrame(null, null, null, taskConfig);
+
+        System.out.println(timeFrame);
+        long earliest = timeFrame.getEarliest();
+        long latest = timeFrame.getLatest();
+        String taskDefiningFilter = taskConfig.getDefiningFilter();
+
+        taskDefiningFilter += " AND (taskId:\"15F679A3\" OR taskId:\"5C3222C7\")";
+
+        BoolQueryBuilder queryBuilder1 = QueryBuilders.boolQuery()
+                .must(QueryBuilders.rangeQuery("@timestamp").gt(earliest).lt(latest))
+                .must(QueryBuilders.queryStringQuery(taskDefiningFilter));
+
+        XContentBuilder contentBuilder = XContentFactory.jsonBuilder().startObject().field("query");
+        queryBuilder1.toXContent(contentBuilder, null);
+
+        contentBuilder.endObject();
+        System.out.println( contentBuilder.string());
+//        POST _search
+//        {
+//            "query" : {
+//            "query_string" : { "query" : "taskId:\"15F679A3\" OR taskId:\"5C3222C7\"" }
+//        }
+    }
 
 	@Test
 	public void testFindTaskConfig() throws IOException {
